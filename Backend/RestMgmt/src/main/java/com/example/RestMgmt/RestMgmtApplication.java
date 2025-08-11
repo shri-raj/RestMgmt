c8ffad1d29884c03096212ca30c757e9420b6e58
Shriraj Patil <patilshriraj2602@gmail.com>
2025-08-11 09:29:49 +0530

    Initial commit

---
+++ Backend/RestMgmt/src/main/java/com/example/RestMgmt/RestMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMgmtApplication.class, args);
+	}
+
+}
---
+++ Backend/RestMgmt/src/test/java/com/example/RestMgmt/RestMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
